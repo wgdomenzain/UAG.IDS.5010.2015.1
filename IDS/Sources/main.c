b23a81eef9d7cef67ba89e178b69a39d1b0f8ce1
porritas <porrasporrasporras@gmail.com>
2015-01-21 14:39:18 -0600

    Initial config
    initial config

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,26 +1,33 @@
 /*
  * main implementation: use this 'C' sample to create your own application
- *
+ * Author: Juan Abraham Porras
  */
-
-
-
-
 
 #include "derivative.h" /* include peripheral declarations */
 
-
+//prototype declaration
+void cfgPorts(void);
+int calculateAverage(int age1, int age2, int age3);
 
 int main(void)
 {
 	int counter = 0;
-	
-	
-	
+	cfgPorts();
+	counter = calculateAverage(1, 2, 3);
 	
 	for(;;) {	   
 	   	counter++;
 	}
 	
 	return 0;
+}
+
+void cfgPorts(void){
+	
+}
+
+int calculateAverage(int age1, int age2, int age3){
+	int avg = age1 + age2 + age3;
+	avg = result/3;
+	return avg;
 }
