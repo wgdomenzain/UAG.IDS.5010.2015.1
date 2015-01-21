10969e72d76e8f89881ad5dd8c570dd44cbc05e9
elcony23 <ldaa_cony@hotmail.com>
2015-01-21 14:27:39 -0600

    Initial donaldo configuration
    Initial donaldo configuration

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -10,12 +10,15 @@
 #include "derivative.h" /* include peripheral declarations */
 
 
+void cfgporTs(void);
+int calculateAverage(int age1,int age2,int age3);
 
 int main(void)
 {
+	//prototype declaration
 	int counter = 0;
-	
-	
+	cfgporTs();
+	counter = calculateAverage(1,2,3);
 	
 	
 	for(;;) {	   
@@ -23,4 +26,12 @@ int main(void)
 	}
 	
 	return 0;
+}
+void cfgporTs(void){
+	
+}
+int calculateAverage(int age1,int age2,int age3){
+	
+	return 1;
+	
 }
