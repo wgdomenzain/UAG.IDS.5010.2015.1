a56e6b7f40ad4932012e0cbb54773c3555b992f8
AMarielZ <mariel_zamora10@hotmail.com>
2015-01-21 14:27:38 -0600

    Configuración Inicial de Mariel
    cidm

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -9,14 +9,17 @@
 
 #include "derivative.h" /* include peripheral declarations */
 
+//prototype declaration 
 
+void cfgPorts(void);
+int calculateAverage(int age1, int age2, int age3);
 
 int main(void)
 {
 	int counter = 0;
 	
-	
-	
+	cfgPorts();
+	counter = calculateAverage(1,2,3);
 	
 	for(;;) {	   
 	   	counter++;
@@ -24,3 +27,14 @@ int main(void)
 	
 	return 0;
 }
+void cfgPorts(void)
+{
+	
+}
+	
+int calculateAverage(int age1, int age2, int age3)
+{
+	return 1;
+}
+
+
