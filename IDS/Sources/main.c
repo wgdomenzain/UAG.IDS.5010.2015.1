00691aacee386917e34910157869d0b895b92a12
angellsistemas <angellsistemas@gmail.com>
2015-01-21 14:40:36 -0600

    Ejemplo1
    ejemplo1

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,4 +1,6 @@
 /*
+ * Marco Antonio Angel Pérez
+ * 
  * main implementation: use this 'C' sample to create your own application
  *
  */
@@ -13,13 +15,18 @@ int calculateAverage(int age1,int age2,int age3);
 int main(void)
 {
 	int counter = 0;
-
+	int counter2 = 0;
+	
 	cfgPorts();
 	counter=calculateAverage(1,2,3);
 	
 	for(;;) {	   
 	   	counter++;
 	}
+	
+	for(;;) {	   
+		   	counter2++;
+		}
 	
 	return 0;
 }
