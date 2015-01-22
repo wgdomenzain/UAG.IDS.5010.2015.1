38393a35ffc9824b3ce6c79e1b8d17d14eef60c3
CarlosPayan <carlospayan26@gmail.com>
2015-01-22 10:13:10 -0600

    Ahora calcula promedio

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -8,14 +8,14 @@
 
 
 #include "derivative.h" /* include peripheral declarations */
-
-
+//Este codigo fue modificado por Carlos Alejandro "Java" Payan Castellanos
+int promedio(int uno,int dos,int tres);
 
 int main(void)
 {
 	int counter = 0;
 	
-	
+	counter=promedio(1,2,3);
 	
 	
 	for(;;) {	   
@@ -23,4 +23,13 @@ int main(void)
 	}
 	
 	return 0;
+}
+
+int promedio(int uno,int dos,int tres)
+{
+	int total=0;
+	total=uno+dos+tres;
+	total=total/3;
+
+	return total;
 }
