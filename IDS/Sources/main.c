95e0bd27586c946f5480bcd85cbc7ddd479fd8f8
danielsan22 <daniel95_543@hotmail.com>
2015-01-22 10:12:54 -0600

    Avg function
    Contiene una funcion de promedio

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -8,19 +8,26 @@
 
 
 #include "derivative.h" /* include peripheral declarations */
+//Modificado por Daniel Sànchez Ramírez 
 
-
-
+int avg(int x, int y, int z);
 int main(void)
 {
 	int counter = 0;
 	
 	
-	
+	counter = avg(2, 2, 2);
 	
 	for(;;) {	   
 	   	counter++;
 	}
 	
 	return 0;
+}
+int avg(int x, int y, int z){
+	
+	int prom;
+	
+	prom = (x+y+z)/3
+	return prom; 
 }
