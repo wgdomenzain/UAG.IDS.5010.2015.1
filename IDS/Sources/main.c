0a0eaa4400c8832497f6be9c0f91139b0639cae2
Dachb <dachb_10@hotmail.com>
2015-01-22 17:35:12 +0100

    Promedio

---
+++ IDS/Sources/main.c
@@ -0,0 +1,39 @@
+/*
+ * main implementation: use this 'C' sample to create your own application
+ *l
+ */
+
+
+
+
+
+#include "derivative.h" /* include peripheral declarations */
+
+void cfgPorts(void);
+int calculateAverage(int age1,int age2,int age3); 
+
+int main(void)
+{
+	int counter = 0;
+	
+	cfgPorts();
+	counter = calculateAverage(1,2,3);
+	
+	
+	
+	for(;;) {	   
+	   	counter++;
+	}
+	
+	return 0;
+}
+void cfgPorts(void)
+{
+	
+}
+int calculateAverage(int age1,int age2,int age3)
+{
+	int res;
+	res=((age1+age2+age3)/3);
+	return res;
+}
