e5acbd3e7592b42c57948577e6959d1c84b729f4
jessicaarvz <j.arvizutr@gmail.com>
2015-01-28 14:50:26 -0600

    Jessica

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -2,44 +2,42 @@
  * main implementation: use this 'C' sample to create your own application
  *
  */
-
-
-
-
-
 #include "derivative.h" /* include peripheral declarations */
-//prototipe declaration
-
-//  _           _        
-// | |         | |       
-// | |__   ___ | | __ _  
-// | '_ \ / _ \| |/ _` | 
-// | | | | (_) | | (_| | 
- //|_| |_|\___/|_|\__,_| 
-                      
+//#define portB GPIOB_PDOR// Declara una variable 
 void cfgPorts(void);
-int calculateAverage (int age1,int age2,int age3);
 
 int main(void)
 {
-	int counter = 0;
 	
+	//Port Configuration 
 	cfgPorts();
-	counter = calculateAverage(1,2,3);
+	// Turn on Led
+	GPIOB_PDOR = 0xFFFBFFFF; //Rojo
+	GPIOB_PDOR = 0xFFF3FFFF; //Verde
+	GPIOD_PDOR = 0xFFFFFFFD; //azul
 	
-	for(;;) {	   
-	   	counter++;
+	for(;;) 
+	{
+	   
 	}
 	
 	return 0;
 }
-
 void cfgPorts(void)
 {
+	//Turn on Clock for portB and portC
 	
-}
-
-int calculateAverage (int age1,int age2,int age3)
-{
-	return 1;
+	SIM_SCGC5 =  SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	
+	//Select Port mode : set Pins of PORTs an GPIo(in and out of general )
+	
+	PORTB_PCR18  = PORT_PCR_MUX(1);
+	PORTB_PCR19  = PORT_PCR_MUX(1);
+	PORTD_PCR1   = PORT_PCR_MUX(1); 
+	//Configure PortB as Output
+	
+	GPIOB_PDDR = 0xFFFFFFFF;     // 1111 1111 1111 .... 
+	GPIOD_PDDR = 0xFFFFFFFF;
 }
