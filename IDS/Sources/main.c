6f59d98e428d0c4ee753073d397f0aef628e2dab
AMarielZ <mariel_zamora10@hotmail.com>
2015-01-28 14:41:38 -0600

    Red&Green&Blue

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -2,39 +2,44 @@
  * main implementation: use this 'C' sample to create your own application
  *
  */
-
-
-
-
-
 #include "derivative.h" /* include peripheral declarations */
+#define portB GPIOB_PDOR
 
 //prototype declaration 
-
 void cfgPorts(void);
-int calculateAverage(int age1, int age2, int age3);
 
 int main(void)
 {
-	int counter = 0;
-	
+	//Ports Configuration
 	cfgPorts();
-	counter = calculateAverage(1,2,3);
+	//Turn on Led
+	//GPIOB_PDOR = 0xFFFBFFFF; Red
+	portB = 0xFFFBFFFF;
+	GPIOB_PDOR &= 0xFFF7FFFF;
+	GPIOD_PDOR = 0xFFFFFFFD; 
+	
 	
 	for(;;) {	   
-	   	counter++;
+	   	
 	}
 	
 	return 0;
 }
-void cfgPorts(void)
-{
-	
-}
-	
-int calculateAverage(int age1, int age2, int age3)
-{
-	return 1;
-}
 
+void cfgPorts(void){
+	//Turn on clock for portB
+	SIM_SCGC5 =  SIM_SCGC5_PORTB_MASK;  // 0x00000400;
+	SIM_SCGC5 |=  SIM_SCGC5_PORTA_MASK; 
+	SIM_SCGC5 |=  SIM_SCGC5_PORTD_MASK; 
 
+	
+	//Activate pin mode
+	PORTB_PCR18 = PORT_PCR_MUX(1);
+	PORTB_PCR19 = PORT_PCR_MUX(1);
+	PORTD_PCR1  = PORT_PCR_MUX(1);
+	
+	//Configure portB as output	
+	GPIOB_PDDR = 0xFFFFFFFF;
+	GPIOB_PDDR = 0xFFFFFFFF;  //0x00040000	
+	GPIOD_PDDR = 0xFFFFFFFF;
+}
