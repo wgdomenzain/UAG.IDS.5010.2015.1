574621f5c80c2df534bd6985ea9b492793ebdc2c
alandm7 <alandemiguel@gmail.com>
2015-01-28 23:40:55 -0600

    codigo LED blaco
    practica 2

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -10,29 +10,30 @@ int main(void)
 	cfgPorts();
 	
 	//GPIOB_PDOR = 0xFFFBFFFF;// ASI ACTIVAMOS EL LED POQUE EL LED PRENDE CON 0     ROJO PRENDE
-	//GPIOB_PDOR = 0xFFF7FFFF;   .PRENDE LED VERDE
+	//GPIOB_PDOR = 0xFFF7FFFF;   .PRENDE VERDE
 	portB = 0xFFFBFFFF;
 	GPIOB_PDOR &= 0xFFF7FFFF;
 	GPIOD_PDOR = 0xFFFFFFFD;
-	for(;;) {	   
-	   	
+	for(;;) 
+	{
 	}
 	
 	return 0;
 }
-void cfgPorts(void){
-	
+void cfgPorts(void)
+{
 	SIM_SCGC5 =  SIM_SCGC5_PORTB_MASK;
 	SIM_SCGC5 |=  SIM_SCGC5_PORTA_MASK; 
 	SIM_SCGC5 |=  SIM_SCGC5_PORTD_MASK;
 	
 	//ACTIVA EL MODO DEL PIN 
-	PORTB_PCR18 = PORT_PCR_MUX(1);// 1 es el modulo de la tabla la columna de funcionalidad
+	PORTB_PCR18 = PORT_PCR_MUX(1);// 1 es el modulo de la tabla la clumna de funcionalidad
 	PORTB_PCR19 = PORT_PCR_MUX(1);
 	PORTD_PCR1  = PORT_PCR_MUX(1);// MODO 1
 	
-	//CONFIGURE PORT B AS OUTPUT buscar capitulo GPIO
+	//CONFIGURE PORT B AS OUTPUT           para encontrar lo siguiente buscar capitulo GPIO
 	GPIOB_PDDR = 0xFFFFFFFF;
 	GPIOB_PDDR = 0xFFFFFFFF;
 	GPIOD_PDDR = 0xFFFFFFFF;
 }
+//Alan De Miguel Garcia  #2508782 IDS
