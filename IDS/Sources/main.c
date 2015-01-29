3d9b0df28fd79e63e68e14bebb410c6586ca2a26
alandm7 <alandemiguel@gmail.com>
2015-01-28 21:30:45 -0600

    Codigo led blanco
    Pract 2

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,15 +1,16 @@
-//Alberto Bautista
-//Ingenieria de Software
-
-#include "derivative.h" 
+/*
+ * main implementation: use this 'C' sample to create your own application
+ *
+ */
+#include "derivative.h" /* include peripheral declarations */
 #define portB GPIOB_PDOR
-
 void cfgPorts(void);
-
 int main(void)
 {
 	cfgPorts();
 	
+	//GPIOB_PDOR = 0xFFFBFFFF;// ASI ACTIVAMOS EL LED POQUE EL LED PRENDE CON 0     ROJO PRENDE
+	//GPIOB_PDOR = 0xFFF7FFFF;PRENDE VERDE
 	portB = 0xFFFBFFFF;
 	GPIOB_PDOR &= 0xFFF7FFFF;
 	GPIOD_PDOR = 0xFFFFFFFD;
@@ -25,12 +26,12 @@ void cfgPorts(void){
 	SIM_SCGC5 |=  SIM_SCGC5_PORTA_MASK; 
 	SIM_SCGC5 |=  SIM_SCGC5_PORTD_MASK;
 	
-	
-	PORTB_PCR18 = PORT_PCR_MUX(1);
+	//ACTIVA EL MODO DEL PIN 
+	PORTB_PCR18 = PORT_PCR_MUX(1);// 1 es el modulo de la tabla la columna de funcionalidad
 	PORTB_PCR19 = PORT_PCR_MUX(1);
-	PORTD_PCR1  = PORT_PCR_MUX(1);
+	PORTD_PCR1  = PORT_PCR_MUX(1);// MODO 1
 	
-
+	//CONFIGURE PORT B AS OUTPUT buscar capitulo GPIO
 	GPIOB_PDDR = 0xFFFFFFFF;
 	GPIOB_PDDR = 0xFFFFFFFF;
 	GPIOD_PDDR = 0xFFFFFFFF;
