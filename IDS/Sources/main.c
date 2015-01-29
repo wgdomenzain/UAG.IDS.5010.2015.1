aa577e1843ba7b112bff793e4337da81f18209d3
AlfredoDaAs <davilaassadalfredo@gmail.com>
2015-01-28 21:44:50 -0600

    Parpadeo de Led
    En este video se muestra el parpadeo de 1 segundo aproximadamente
    cambiando a los siguientes colores
    https://www.youtube.com/watch?v=RDJb1aqM8jU

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,39 +1,77 @@
-/*
- * main implementation: use this 'C' sample to create your own application
- *
- */
-
-
-
-
-
 #include "derivative.h" /* include peripheral declarations */
 
+#define portB GPIOB_PDOR
+#define portD GPIOD_PDOR
+
 void cfgPorts(void);
-int calculateAverage(int age1,int age2,int age3); 
+void retardo(long ms);
+void apagarPuertoB(void);
+void apagarPuertoD(void);
+
 
 int main(void)
 {
-	int counter = 0;
-	
 	cfgPorts();
-	counter = calculateAverage(1,2,3);
+	apagarPuertoB();
+	apagarPuertoD();
 	
-	
-	
-	for(;;) {	   
-	   	counter++;
+	for(;;) {	  
+		//prende  rojo
+		portB = 0xFFFBFFFF;
+		retardo(2100000);
+		//tiempo apagado
+		apagarPuertoB();
+		retardo(2100000);
+		
+		//prende verde
+		portB = 0xFFF7FFFF;
+		retardo(2100000);
+		//tiempo apagado
+		apagarPuertoB();
+		retardo(2100000);
+		
+		//prende azul
+		portD = 0xFFFFFFFD;
+		retardo(2100000);
+		//tiempo apagado
+		apagarPuertoD();
+		retardo(2100000);
+		
 	}
 	
 	return 0;
 }
-void cfgPorts(void)
-{
+void retardo(long ms){
+	do
+	{
+		ms--;
+	}
+	while(ms!=0);
 	
 }
-int calculateAverage(int age1,int age2,int age3)
+void apagarPuertoB(void)
 {
-	int res;
-	res=(age1+age2+age3)/3;
-	return res;
+	portB = 0xFFFFFFFF; // apagar leds rojo y verde
+}
+void apagarPuertoD(void)
+{
+	portD = 0xFFFFFFFF; // apagar led azul
+}
+
+void cfgPorts(void)
+{
+	//Activar Reloj Puerto A, B y D
+	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	
+	// Activar pines como GPIO
+	PORTB_PCR18= PORT_PCR_MUX(1);
+	PORTB_PCR19= PORT_PCR_MUX(1);
+	PORTD_PCR1= PORT_PCR_MUX(1); 
+	
+	
+	// Configurar direccion como salida de los puertos
+	GPIOB_PDDR= 0xFFFFFFFF;
+	GPIOD_PDDR= 0xFFFFFFFF;
 }
