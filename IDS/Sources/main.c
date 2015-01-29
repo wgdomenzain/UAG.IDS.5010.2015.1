031267688883e102296babdf934b081cc564a195
Dachb <dachb_10@hotmail.com>
2015-01-29 04:10:08 +0100

    Cambio_Colores_Leds

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -2,40 +2,61 @@
  * main implementation: use this 'C' sample to create your own application
  *
  */
-
 #include "derivative.h" /* include peripheral declarations */
-#define portB GPIOB_PDOR
 
 void cfgPorts(void);
-
+void Delay(long ms);
+void apagarLeds(void);
 int main(void)
 {
 	cfgPorts();
+	apagarLeds();
 	
-	//GPIOB_PDOR = 0xFFFBFFFF;// ASI ACTIVAMOS EL LED POQUE EL LED PRENDE CON 0     ROJO PRENDE
-	//GPIOB_PDOR = 0xFFF7FFFF;PRENDE VERDE
-	portB = 0xFFFBFFFF;
-	GPIOB_PDOR &= 0xFFF7FFFF;
-	GPIOD_PDOR = 0xFFFFFFFD;
 	for(;;) {	   
-	   	
+		GPIOB_PDOR = 0xFFFBFFFF; 
+		Delay(1800000);
+		apagarLeds();
+		Delay(1800000);
+		GPIOB_PDOR = 0xFFF7FFFF; 
+		Delay(1800000);
+		apagarLeds();
+		Delay(1800000);
+		GPIOD_PDOR = 0xFFFFFFFD; 
+		Delay(1800000);
+		apagarLeds();
+		Delay(1800000);
+		
 	}
 	
 	return 0;
 }
-void cfgPorts(void){
+
+void apagarLeds(void)
+{
+	GPIOB_PDOR = 0xFFFFFFFF; 
+	GPIOD_PDOR = 0xFFFFFFFF; 
+}
+void cfgPorts(void)
+{
 	
-	SIM_SCGC5 =  SIM_SCGC5_PORTB_MASK;
-	SIM_SCGC5 |=  SIM_SCGC5_PORTA_MASK; 
-	SIM_SCGC5 |=  SIM_SCGC5_PORTD_MASK;
+	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+
+	PORTB_PCR18= PORT_PCR_MUX(1);
+	PORTB_PCR19= PORT_PCR_MUX(1);
+	PORTD_PCR1= PORT_PCR_MUX(1); 
 	
-	//ACTIVA EL MODO DEL PIN 
-	PORTB_PCR18 = PORT_PCR_MUX(1);// 1 es el modulo de la tabla la clumna de funcionalidad
-	PORTB_PCR19 = PORT_PCR_MUX(1);
-	PORTD_PCR1  = PORT_PCR_MUX(1);// MODO 1
+	GPIOB_PDDR= 0xFFFFFFFF;
+	GPIOD_PDDR= 0xFFFFFFFF;
+}
+
+void Delay(long ms)
+{
+	do
+	{
+		ms--;
+	}
+	while(ms!=0);
 	
-	//CONFIGURE PORT B AS OUTPUT buscar capitulo GPIO
-	GPIOB_PDDR = 0xFFFFFFFF;
-	GPIOB_PDDR = 0xFFFFFFFF;
-	GPIOD_PDDR = 0xFFFFFFFF;
 }
