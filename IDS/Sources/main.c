c891ee38a3d719a487ff2ded790434140c4f0e53
danielsan22 <daniel95_543@hotmail.com>
2015-01-29 11:04:42 -0600

    Led
    Prende el led de colores parpadeando.

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -11,23 +11,69 @@
 #define portB GPIOB_PDOR
 
 void cfgPorts(void);
-
+void delay(void);
+void limpiar(void);
 int main(void)
 {
 	cfgPorts();
-	
+	//ROJO
 	//GPIOB_PDOR = 0xFFFBFFFF; //PRENDE ROJO
-	//GPIOB_PDOR = 0xFFF7FFFF; //PRENDE VERDE
-	//GPIOB_PDOR = 0xFFF3FFFF; //PRENDE Amarillo
-	portB = 0x00000000;
-	portB = 0xFFFFFFFD;		
+	//GPIOD_PDOR = 0xFFFFFFFF;
+	//ROJO
+	//VERDE
+	GPIOD_PDOR = 0xFFFFFFFF;
+	GPIOB_PDOR = 0xFFF7FFFF;
+	delay();
+	GPIOB_PDOR = 0xFFFFFFFF;
+	GPIOD_PDOR = 0xFFFFFFFF;
+	//VERDE
 	
-	for(;;) {	  
+	
+	
+	
+	//Azul
+	//GPIOB_PDOR = 0xFFFFFFFFFF;
+	//GPIOD_PDOR = 0xFFFFFFFD;
+	//Azul
+	for(;;) {	
+		//Limpia
+		GPIOB_PDOR = 0xFFFFFFFF;
+		GPIOD_PDOR = 0xFFFFFFFF;
+		//ROJO
+		GPIOB_PDOR = 0xFFFBFFFF;
+		GPIOD_PDOR = 0xFFFFFFFF;
+		delay();
+		limpiar();
+		delay();
+		
+		//VERDE
+		GPIOD_PDOR = 0xFFFFFFFF;
+		GPIOB_PDOR = 0xFFF7FFFF;
+		delay();
+		limpiar();
+		delay();
+		//AZUL
+		GPIOB_PDOR = 0xFFFFFFFF;
+		GPIOD_PDOR = 0xFFFFFFFD;
+		delay();
+		limpiar();
+		delay();
 	   	
 	}
 	
 	return 0;
 }
+void delay(void){
+	int i;
+	for(i = 0; i<1600000;i++){
+		
+	}
+}
+void limpiar(void){
+	GPIOB_PDOR = 0xFFFFFFFF;
+	GPIOD_PDOR = 0xFFFFFFFF;
+}
+
 void cfgPorts(void){
 	
 	SIM_SCGC5 =  SIM_SCGC5_PORTB_MASK;
@@ -43,3 +89,6 @@ void cfgPorts(void){
 	GPIOD_PDDR = 0xFFFFFFFF;
 	
 }
+//GPIOB_PDOR = 0xFFF3FFFF; //PRENDE Amarillo
+//portB = 0x00000000;
+//portB = 0xFFFFFFFD;	
