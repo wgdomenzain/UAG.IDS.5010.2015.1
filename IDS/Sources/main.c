9153042a8370f800096dd3b64e98bad2222c5008
angellsistemas <angellsistemas@gmail.com>
2015-01-28 23:09:45 -0600

    Led parpadenate
    Los leds cambian de color cada segundo y hacen una pausa

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -7,23 +7,49 @@
 
 #include "derivative.h" /* include peripheral declarations */
 
+
 //prototype declaration
 void cfgPorts(void);
+void delay(void);
+
 
 int main(void)
 {
-	//port configuration
 	cfgPorts();
-	//TURN ON LED RED
-	//GPIOB_PDOR = 0xFFF3FFFF;
-	//TURN ON LED GREEN
-	//GPIOB_PDOR = 0xFFF3FFFF;
-	//TURN ON LED BLUE
-	GPIOD_PDOR = 0xFFFFFFFD;
+
+	//apagar puertos
 		
+	
+		//cerrar el puerto 19 para que no encienda la luz verde
+	GPIOD_PDOR = 0xFFFFFFFF;
+	
 	for(;;) 
 	{	   
-	
+		//encender led rojo por puerto B18
+		GPIOB_PDOR = 0xFFFBFFFF;
+					//esperar un segundo 
+		delay();
+						//apagar led rojo por puerto B18
+		GPIOB_PDOR = 0xFFFFFFFF;
+					//esperar un segundo 
+		delay();
+		//encender led verde por puerto B19 previamente apagando el rojo para que no se convierta en magenta
+		GPIOB_PDOR = 0xFFF7FFFF;
+					//esperar un segundo
+		delay();
+						//apago el led verde
+		GPIOB_PDOR = 0xFFFFFFFF;
+					//esperar un segundo
+		delay();
+		////encender led azul por puerto D13 previamente apagando el rojo para que no se convierta en otro color
+		GPIOD_PDOR = 0xFFFFFFFD;
+					//esperar un segundo
+		delay();
+		//apagar led azul
+		GPIOD_PDOR = 0xFFFFFFFF;
+					//esperar un segundo
+		delay();
+		
 	}
 	
 	return 0;
@@ -41,12 +67,24 @@ void cfgPorts(void)
 	PORTB_PCR19 = PORT_PCR_MUX(1);
 	PORTD_PCR1 = PORT_PCR_MUX(1);
 	
-	//Configure PORT B
+	//Configure PORT 
 	GPIOA_PDDR = 0xFFFFFFFF;
 	GPIOB_PDDR = 0xFFFFFFFF; 
 	GPIOD_PDDR = 0xFFFFFFFF; 
 }	
-
+void delay(void){
+	
+	{
+	   int c;
+	   for (c=1;c<1600000;c++)
+	   {
+		   
+	   }
+	   
+	 
+	  
+	}
+ }
 
 
 
