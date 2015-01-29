1377d7c877cf272e0740843b78d357a7acc8e7c4
Dachb <dachb_10@hotmail.com>
2015-01-29 02:36:54 +0100

    Laboratorio02

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,40 +1,41 @@
-/*Author:Alberto Bautista	
+/*Author:Alberto Bautista	 1766491
 	Date: 14/01/15
 	Description: Initial Description
  * main implementation: use this 'C' sample to create your own application
  *
  */
 
-
-
-
-
 #include "derivative.h" /* include peripheral declarations */
-
+#define portB GPIOB_PDOR
 
 void cfgPorts(void);
+
 int main(void)
 {
-	
-	
 	cfgPorts();
-	GPI0B_PDDR = 0x00000000;
 	
 	
+	portB = 0xFFFBFFFF;
+	GPIOB_PDOR &= 0xFFF7FFFF;
+	GPIOD_PDOR = 0xFFFFFFFD;
 	for(;;) {	   
 	   	
 	}
 	
 	return 0;
-	void cfgPorts(void)
-{
+}
+void cfgPorts(void){
+	
 	SIM_SCGC5 =  SIM_SCGC5_PORTB_MASK;
 	SIM_SCGC5 |=  SIM_SCGC5_PORTA_MASK; 
+	SIM_SCGC5 |=  SIM_SCGC5_PORTD_MASK;
+	
 	
 	PORTB_PCR18 = PORT_PCR_MUX(1);
 	PORTB_PCR19 = PORT_PCR_MUX(1);
+	PORTD_PCR1  = PORT_PCR_MUX(1);
 	
-	//CONFIGURE PORTS AS OUTPUT
-	GPI0B_PDDR = 0xFFFFFFFF;
-}
+	GPIOB_PDDR = 0xFFFFFFFF;
+	GPIOB_PDDR = 0xFFFFFFFF;
+	GPIOD_PDDR = 0xFFFFFFFF;
 }
