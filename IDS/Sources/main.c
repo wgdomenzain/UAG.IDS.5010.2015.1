71c4d65b206197a6243d7e841d555d2a62d97cbd
jessicaarvz <j.arvizutr@gmail.com>
2015-01-28 20:26:54 -0600

    1sec_led.

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,43 +1,73 @@
+
 /*
  * main implementation: use this 'C' sample to create your own application
  *
  */
 #include "derivative.h" /* include peripheral declarations */
-//#define portB GPIOB_PDOR// Declara una variable 
+//#define portB GPIOB_PDOR// Para declarar una variable
+#define portB GPIOB_PDOR
+#define portD GPIOD_PDOR
+
 void cfgPorts(void);
+void Delay(long ms);
+void TurnOffLeds(void);
+
 
 int main(void)
 {
-	
-	//Port Configuration 
+	//PortConfiguration
 	cfgPorts();
-	// Turn on Led
-	GPIOB_PDOR = 0xFFFBFFFF; //Rojo
-	GPIOB_PDOR = 0xFFF3FFFF; //Verde
-	GPIOD_PDOR = 0xFFFFFFFD; //azul
+	TurnOffLeds();
 	
-	for(;;) 
-	{
-	   
+	for(;;) {	   
+		portB = 0xFFF7FFFF; //led verde
+		Delay(2000000);
+		TurnOffLeds();
+		Delay(2000000);
+		portD = 0xFFFFFFFD; // led azul
+		Delay(2000000);
+		TurnOffLeds();
+		Delay(2000000);
+		portB = 0xFFFBFFFF; //led rojo
+		Delay(2000000);
+		TurnOffLeds();
+		Delay(2000000);
+		
 	}
 	
 	return 0;
 }
+
+void TurnOffLeds(void)
+{
+	portB = 0xFFFFFFFF; // turn off red&green leds
+	portD = 0xFFFFFFFF; // turn off blue led
+}
+
 void cfgPorts(void)
 {
 	//Turn on Clock for portB and portC
-	
-	SIM_SCGC5 =  SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
 	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
 	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
 	
-	//Select Port mode : set Pins of PORTs an GPIo(in and out of general )
+	//Select Port mode : set Pins of PORTs an GPIo(in and out of general purpose)
+	PORTB_PCR18= PORT_PCR_MUX(1);
+	PORTB_PCR19= PORT_PCR_MUX(1);
+	PORTD_PCR1= PORT_PCR_MUX(1); 
 	
-	PORTB_PCR18  = PORT_PCR_MUX(1);
-	PORTB_PCR19  = PORT_PCR_MUX(1);
-	PORTD_PCR1   = PORT_PCR_MUX(1); 
+	
 	//Configure PortB as Output
+	GPIOB_PDDR= 0xFFFFFFFF;
+	GPIOD_PDDR= 0xFFFFFFFF;
+}
+
+void Delay(long ms)
+{
+	do
+	{
+		ms--;
+	}
+	while(ms!=0);
 	
-	GPIOB_PDDR = 0xFFFFFFFF;     // 1111 1111 1111 .... 
-	GPIOD_PDDR = 0xFFFFFFFF;
 }
