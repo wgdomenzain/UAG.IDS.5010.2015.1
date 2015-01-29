21319a5f695d1d59f1de8f36ecd834ac71d80d1f
porritas <porrasporrasporras@gmail.com>
2015-01-28 22:33:39 -0600

    BLUE & RED LED
    Blue and red LED blink

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -4,37 +4,38 @@
  */
 #include "derivative.h" /* include peripheral declarations */
 #include "stdio.h"
-#define portB GPIOB_PDOR
-#define portD GPIOD_PDOR
 //prototype declaration
 void setClock(void);
 void portMode(void);
 void output(void);
 void cfgPorts(void);
-void delay(long time);
+void delay(int time);
 void setOutputs(void);
-void red(void);
 int main(void){
-	setClock();
-	portMode();
-	GPIOB_PDOR = 0x00000000;
-	GPIOD_PDOR = 0x00000000;
-	output();
-	
-	//GPIOB_PDOR = 0xFFF7FFFF;// Red
-	GPIOB_PDOR = 0xFFF7FFFF;//ROJO ENCENDIDO - VERDE APAGADO
-	for(;;){
-		break;
-	}	
-	GPIOB_PDOR = 0xFFF0FFFF;//ROJO APAGADO - VERDE APAGADO
-	for(;;){
-		break;
-	}	
-	GPIOD_PDOR = 0xFFFFFFFD;//BLUE
-	
-	for(;;){
-	}	
-	return 0;
+		setClock();
+		portMode();
+		output();
+		GPIOB_PDOR = 0xFFFFFFFF;
+		GPIOB_PDOR = 0xFFFFFFFF;
+		GPIOD_PDOR = 0xFFFFFFFF;
+		//Turn on Led
+		//GPIOB_PDOR = 0xFFF3FFFF; //amrillo
+		//GPIOB_PDOR = 0xFFF7FFFF; //verde
+		//GPIOB_PDOR = 0xFFFBFFFF; //rojo
+		for(;;){
+			GPIOB_PDOR = 0xFFFBFFFF;//ROJO
+			delay(500);
+			GPIOB_PDOR = 0xFFFFFFFF;
+			delay(500);
+			GPIOD_PDOR = 0xFFFFFFFD;
+			delay(500);
+			GPIOD_PDOR = 0xFFFFFFFF;
+			delay(500);
+			//GPIOB_PDOR = 0xFFFFFFFF;
+			//GPIOD_PDOR = 0xFFFFFFFD;//AZUL
+			//GPIOB_PDOR = 0xFFFFFFFF;
+		}
+		return 0;
 }
 void setClock(void){
 	//Ports configuration
@@ -48,12 +49,12 @@ void portMode(void){
 	//select port mode: set pins of PORTB as GPIO by using ALT01
 	PORTB_PCR18 = PORT_PCR_MUX(1);//RED
 	PORTB_PCR19 = PORT_PCR_MUX(1);//GREEN
-	PORTD_PCR1 = PORT_PCR_MUX(1);//BLUE
+	PORTD_PCR1 = PORT_PCR_MUX(1);//BLUE		
 }
 void output(void){
 	//configure port B and D as output
 	GPIOB_PDDR = 0xFFFFFFFF;//0xFFF3FFFF==RED/GREEN DOWN - 0xFFF7FFFF==RED ON - 0xFFFBFFFF==GREEN ON
-	GPIOD_PDDR = 0xFFFFFFFF;//0xFFFFFFF0==BLUE DOWN
+	GPIOD_PDDR = 0x00000002;
 }
 void cfgPorts(void){
 	//turn on clock for portB and portD
@@ -69,8 +70,14 @@ void cfgPorts(void){
 	GPIOB_PDDR = 0xFFFFFFFF; //1111 1111 1111 1111...
 	GPIOD_PDDR = 0xFFFFFFF0;//
 }
-void delay(long time){
-	while(time>0){
+void delay(int time){
+	int t;
+	while(time){
+		t = 100;
+		while(t){
+			t--;
+		}
 		time--;
 	}
+	return;
 }
