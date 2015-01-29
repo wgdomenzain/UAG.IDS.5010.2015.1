0c822b70fb42416d131e3b4e8bd2337eba7d9fb5
porritas <porrasporrasporras@gmail.com>
2015-01-28 18:49:17 -0600

    Pruebas de colores
    Identificando el encendido de distintos colores, sin implementar método
    delay(). Visualiza cada paso con un método distinto.

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -3,45 +3,74 @@
  * Author: Juan Abraham Porras
  */
 #include "derivative.h" /* include peripheral declarations */
+#include "stdio.h"
 #define portB GPIOB_PDOR
+#define portD GPIOD_PDOR
 //prototype declaration
+void setClock(void);
+void portMode(void);
+void output(void);
 void cfgPorts(void);
-int calculateAverage(int age1, int age2, int age3);
+void delay(long time);
+void setOutputs(void);
+void red(void);
 int main(void){
-	//Ports configuration
-	cfgPorts();
-	//GPIOB_PDOR = 0xFFFBFFFF; Red
-	//GPIOB_PDOR = 0xFFF7FFFF;GREEN pin 19
-	//GPIOB_PDOR = 0xFFF3FFFF;//Yellow
-	GPIOD_PDOR = 0xFFFFFFFD;//BLUE
-	portB = 0xFFF3FFFF;//RED & GREEN
-	for(;;) {	   
-	   	
-	}
+	setClock();
+	portMode();
+	GPIOB_PDOR = 0x00000000;
+	GPIOD_PDOR = 0x00000000;
+	output();
 	
+	//GPIOB_PDOR = 0xFFF7FFFF;// Red
+	GPIOB_PDOR = 0xFFF7FFFF;//ROJO ENCENDIDO - VERDE APAGADO
+	for(;;){
+		break;
+	}	
+	GPIOB_PDOR = 0xFFF0FFFF;//ROJO APAGADO - VERDE APAGADO
+	for(;;){
+		break;
+	}	
+	GPIOD_PDOR = 0xFFFFFFFD;//BLUE
+	
+	for(;;){
+	}	
 	return 0;
 }
-
-void cfgPorts(void){
-	//turn on clock for portB and portA and portD
+void setClock(void){
+	//Ports configuration
+	//cfgPorts();
+	//turn on clock for portB and portD
 	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
-	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
 	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
-	//x|=0x01 es lo mismo que...
-	//x=x|0x01
-	//0x02=0000 0010
-	//     0000 0001 OR
-	//     0000 0011
-	//select port mode: set pins of PORTB as GPIO by using ALT01
-	PORTB_PCR18 = PORT_PCR_MUX(1);
-	PORTB_PCR19 = PORT_PCR_MUX(1);
-	PORTD_PCR1 = PORT_PCR_MUX(1);
-	//configure port b as output
-	GPIOB_PDDR = 0xFFFFFFFF; //1111 1111 1111 1111...
-	GPIOD_PDDR = 0xFFFFFFFF;
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
 }
-int calculateAverage(int age1, int age2, int age3){
-	int avg = age1 + age2 + age3;
-	avg = avg/3;
-	return avg;
+void portMode(void){
+	//select port mode: set pins of PORTB as GPIO by using ALT01
+	PORTB_PCR18 = PORT_PCR_MUX(1);//RED
+	PORTB_PCR19 = PORT_PCR_MUX(1);//GREEN
+	PORTD_PCR1 = PORT_PCR_MUX(1);//BLUE
+}
+void output(void){
+	//configure port B and D as output
+	GPIOB_PDDR = 0xFFFFFFFF;//0xFFF3FFFF==RED/GREEN DOWN - 0xFFF7FFFF==RED ON - 0xFFFBFFFF==GREEN ON
+	GPIOD_PDDR = 0xFFFFFFFF;//0xFFFFFFF0==BLUE DOWN
+}
+void cfgPorts(void){
+	//turn on clock for portB and portD
+	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
+
+	//select port mode: set pins of PORTB as GPIO by using ALT01
+	PORTB_PCR18 = PORT_PCR_MUX(1);//RED
+	PORTB_PCR19 = PORT_PCR_MUX(1);//GREEN
+	PORTD_PCR1 = PORT_PCR_MUX(1);//BLUE
+	//configure port B and D as output
+	GPIOB_PDDR = 0xFFFFFFFF; //1111 1111 1111 1111...
+	GPIOD_PDDR = 0xFFFFFFF0;//
+}
+void delay(long time){
+	while(time>0){
+		time--;
+	}
 }
