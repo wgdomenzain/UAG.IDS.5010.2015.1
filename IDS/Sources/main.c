cef055fd5e699281ae7e7b2c659bef7d3420172f
Dachb <dachb_10@hotmail.com>
2015-01-29 03:56:49 +0100

    nada

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,6 +1,4 @@
-/*Author:Alberto Bautista	 1766491
-	Date: 14/01/15
-	Description: Initial Description
+/*
  * main implementation: use this 'C' sample to create your own application
  *
  */
@@ -14,7 +12,8 @@ int main(void)
 {
 	cfgPorts();
 	
-	
+	//GPIOB_PDOR = 0xFFFBFFFF;// ASI ACTIVAMOS EL LED POQUE EL LED PRENDE CON 0     ROJO PRENDE
+	//GPIOB_PDOR = 0xFFF7FFFF;PRENDE VERDE
 	portB = 0xFFFBFFFF;
 	GPIOB_PDOR &= 0xFFF7FFFF;
 	GPIOD_PDOR = 0xFFFFFFFD;
@@ -30,11 +29,12 @@ void cfgPorts(void){
 	SIM_SCGC5 |=  SIM_SCGC5_PORTA_MASK; 
 	SIM_SCGC5 |=  SIM_SCGC5_PORTD_MASK;
 	
-	
-	PORTB_PCR18 = PORT_PCR_MUX(1);
+	//ACTIVA EL MODO DEL PIN 
+	PORTB_PCR18 = PORT_PCR_MUX(1);// 1 es el modulo de la tabla la clumna de funcionalidad
 	PORTB_PCR19 = PORT_PCR_MUX(1);
-	PORTD_PCR1  = PORT_PCR_MUX(1);
+	PORTD_PCR1  = PORT_PCR_MUX(1);// MODO 1
 	
+	//CONFIGURE PORT B AS OUTPUT buscar capitulo GPIO
 	GPIOB_PDDR = 0xFFFFFFFF;
 	GPIOB_PDDR = 0xFFFFFFFF;
 	GPIOD_PDDR = 0xFFFFFFFF;
