69bd95b2b29ef2b518db3bef500d83a0af758d7a
AMarielZ <mariel_zamora10@hotmail.com>
2015-02-04 11:02:31 -0600

    Button R G and B

---
+++ Boton/FLASH/Sources/main.d
@@ -0,0 +1,40 @@
+Sources/main.o: ../Sources/main.c \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ C:/UAG/Projects/Boton/Project_Headers/derivative.h \
+ C:/UAG/Projects/Boton/Project_Headers/MKL25Z4.h \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+C:/UAG/Projects/Boton/Project_Headers/derivative.h:
+
+C:/UAG/Projects/Boton/Project_Headers/MKL25Z4.h:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Users/Ana\ Mariel/Desktop/CW\ MCU\ v10.4/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
