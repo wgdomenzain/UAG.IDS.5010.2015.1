7427d2ba0af0f540d58f90c7294432f4eb761978
AlfredoDaAs <davilaassadalfredo@gmail.com>
2015-02-04 00:21:36 -0600

    LED RGB con Push Buttons
    LED RGB con Push Buttons

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,42 +1,58 @@
 #include "derivative.h" /* include peripheral declarations */
-
-#define portB GPIOB_PDOR
-#define portD GPIOD_PDOR
+#define portB 			GPIOB_PDOR
+#define portBIn			GPIOB_PDIR
+#define portD 			GPIOD_PDOR
+#define turnRedLedOn 	0xFFFBFFFF
+#define turnGreenLedOn 	0xFFF7FFFF
+#define turnBlueLedOn 	0xFFFFFFFD
+#define turnLedsOff 	0xFFFFFFFF
+//time constants
+#define n1Sec 			1800000
+#define n2Sec			n1Sec*2
+#define nHalfSec		n1Sec/2
 
 void cfgPorts(void);
 void retardo(long ms);
-void apagarPuertoB(void);
-void apagarPuertoD(void);
 
 
 int main(void)
 {
 	cfgPorts();
-	apagarPuertoB();
-	apagarPuertoD();
+	portB = turnLedsOff;
+	portD = turnLedsOff;
 	
-	for(;;) {	  
+	for(;;) {
+		if(!(portBIn & 0x1)){
+			portB = turnRedLedOn;
+		}else if(!(portBIn & 0x2)){
+			portB = turnGreenLedOn;
+		}else if(!(portBIn & 0x4)){
+			portD = turnBlueLedOn;
+		}
+		portB = turnLedsOff;
+		portD = turnLedsOff;
+		/*
 		//prende  rojo
-		portB = 0xFFFBFFFF;
-		retardo(2100000);
+		portB = turnRedLedOn;
+		retardo(n1Sec);
 		//tiempo apagado
-		apagarPuertoB();
-		retardo(2100000);
+		portB = turnLedsOff;
+		retardo(n1Sec);
 		
 		//prende verde
-		portB = 0xFFF7FFFF;
-		retardo(2100000);
+		portB = turnGreenLedOn;
+		retardo(n1Sec);
 		//tiempo apagado
-		apagarPuertoB();
-		retardo(2100000);
+		portB = turnLedsOff;
+		retardo(n1Sec);
 		
 		//prende azul
-		portD = 0xFFFFFFFD;
-		retardo(2100000);
+		portD = turnBlueLedOn;
+		retardo(n1Sec);
 		//tiempo apagado
-		apagarPuertoD();
-		retardo(2100000);
-		
+		portD = turnLedsOff;
+		retardo(n1Sec);
+		*/
 	}
 	
 	return 0;
@@ -49,15 +65,6 @@ void retardo(long ms){
 	while(ms!=0);
 	
 }
-void apagarPuertoB(void)
-{
-	portB = 0xFFFFFFFF; // apagar leds rojo y verde
-}
-void apagarPuertoD(void)
-{
-	portD = 0xFFFFFFFF; // apagar led azul
-}
-
 void cfgPorts(void)
 {
 	//Activar Reloj Puerto A, B y D
@@ -66,12 +73,16 @@ void cfgPorts(void)
 	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
 	
 	// Activar pines como GPIO
+	PORTB_PCR0= PORT_PCR_MUX(1); //Activando GPIO 0, 1 y 2
+	PORTB_PCR1= PORT_PCR_MUX(1);
+	PORTB_PCR2= PORT_PCR_MUX(1);
+	
 	PORTB_PCR18= PORT_PCR_MUX(1);
 	PORTB_PCR19= PORT_PCR_MUX(1);
 	PORTD_PCR1= PORT_PCR_MUX(1); 
 	
 	
-	// Configurar direccion como salida de los puertos
-	GPIOB_PDDR= 0xFFFFFFFF;
+	// Configure PORTB from 31 to 3 As Output and B0 to B2 as Input
+	GPIOB_PDDR= 0xFFFFFFF8; //1000 = 8
 	GPIOD_PDDR= 0xFFFFFFFF;
 }
