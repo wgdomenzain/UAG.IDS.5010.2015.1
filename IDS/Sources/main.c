d3d8c76253e0206775db71a667ed14fbcaa76672
jessicaarvz <j.arvizutr@gmail.com>
2015-02-04 10:48:18 -0600

    Push buttons y micro.

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,64 +1,91 @@
-
 /*
  * main implementation: use this 'C' sample to create your own application
  *
  */
 #include "derivative.h" /* include peripheral declarations */
-//#define portB GPIOB_PDOR// Para declarar una variable
 #define portB GPIOB_PDOR
+#define portBIn GPIOB_PDIR
 #define portD GPIOD_PDOR
+#define turnRedLedOn 0xFFFBFFFF
+#define turnBlueLedOn 0xFFFFFFFD
+#define turnGreenLedOn 0xFFF7FFFF
+#define turnLedsOff  0xFFFFFFFF
+#define oneSec 1600000
+
 
 void cfgPorts(void);
 void Delay(long ms);
-void TurnOffLeds(void);
 
 
 int main(void)
-{
-	//PortConfiguration
+					{
 	cfgPorts();
-	TurnOffLeds();
+	portB=turnLedsOff;
+	portD=turnLedsOff;
+	Delay(oneSec);
 	
-	for(;;) {	   
-		portB = 0xFFF7FFFF; //led verde
-		Delay(2000000);
-		TurnOffLeds();
-		Delay(2000000);
-		portD = 0xFFFFFFFD; // led azul
-		Delay(2000000);
-		TurnOffLeds();
-		Delay(2000000);
-		portB = 0xFFFBFFFF; //led rojo
-		Delay(2000000);
-		TurnOffLeds();
-		Delay(2000000);
+	for(;;) {
+		
+		if(!(portBIn & (0x00000001)))
+		{
+			portB=turnLedsOff;
+			portD=turnLedsOff;
+			portB=turnRedLedOn;
+		}
+		if(!(portBIn & (0x00000002)))
+		{
+			portB=turnGreenLedOn;
+		}
+		if(!(portBIn & (0x00000004)))
+		{
+			portD=turnBlueLedOn;
+		}
+		portB=turnLedsOff;
+		portD=turnLedsOff;
+		/*else
+		{
+			portB=turnLedsOff;
+		}
+		*/
+		/*
+		portB = 0xFFFBFFFF; // encender led rojo
+		Delay(1800000);
+		apagarLeds();
+		Delay(1800000);
+		portB = 0xFFF7FFFF; // encender led verde
+		Delay(1800000);
+		apagarLeds();
+		Delay(1800000);
+		portD = 0xFFFFFFFD; // encender el azul
+		Delay(1800000);
+		apagarLeds();
+		Delay(1800000);*/
 		
 	}
 	
 	return 0;
 }
 
-void TurnOffLeds(void)
-{
-	portB = 0xFFFFFFFF; // turn off red&green leds
-	portD = 0xFFFFFFFF; // turn off blue led
-}
 
 void cfgPorts(void)
 {
-	//Turn on Clock for portB and portC
+	// TURN ON CLOCK
 	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
 	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
 	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
 	
-	//Select Port mode : set Pins of PORTs an GPIo(in and out of general purpose)
+	// SELECT PORT MODE: SET PINS OF PORTB AS GPIO
+	PORTB_PCR0= PORT_PCR_MUX(1);
+	PORTB_PCR1= PORT_PCR_MUX(1);
+	PORTB_PCR2= PORT_PCR_MUX(1);
+	
 	PORTB_PCR18= PORT_PCR_MUX(1);
 	PORTB_PCR19= PORT_PCR_MUX(1);
 	PORTD_PCR1= PORT_PCR_MUX(1); 
 	
 	
-	//Configure PortB as Output
-	GPIOB_PDDR= 0xFFFFFFFF;
+	// CONFIGURE PORTB FROM 31 TO 3 AS OUTPUT , 0-2 AS INPUT
+	GPIOB_PDDR= 0xFFFFFFF8;
 	GPIOD_PDDR= 0xFFFFFFFF;
 }
 
