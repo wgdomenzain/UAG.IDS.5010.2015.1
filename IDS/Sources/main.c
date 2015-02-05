da15ff93516ea0f302fdfbd4c2c8a3139547928c
alandm7 <alandemiguel@gmail.com>
2015-02-05 10:18:34 -0600

    Codigo para Encender el Led mediante 3 botones
    practica 3

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,74 +1,100 @@
-/* 
-  * main implementation: use this 'C' sample to create your own application 
-  * 
-  */ 
- #include "derivative.h" /* include peripheral declarations */ 
- #define portB GPIOB_PDOR 
+/*
+ * main implementation: use this 'C' sample to create your own application
+ *
+ */
+#include "derivative.h" /* include peripheral declarations */
 
-void cfgPorts(void); 
-void apagarLeds(void);
-void Retardo(long miliseg); 
- 
+#define portBIn GPIOB_PDIR
+#define portB GPIOB_PDOR
+#define portD GPIOD_PDOR
+#define turnRedLedOn 0xFFFBFFFF
+#define turnBlueLedOn 0xFFFFFFFD
+#define turnGreenLedOn 0xFFF7FFFF
+#define turnLedsOff  0xFFFFFFFF
+#define oneSec 1600000
 
 
- int main(void) 
-{ 
- 	cfgPorts(); 
- 	apagarLeds(); 
-	 
-	for(;;) //FOR INFINITO
-	{	    
-		portB = 0xFFFBFFFF; // 0xFFFBFFFF ROJO
-		Retardo(2500000); //
-		apagarLeds(); 
-		Retardo(2500000); 
-		portB = 0xFFF7FFFF; // 0xFFF7FFFF VERDE
-		Retardo(2500000); 
-		apagarLeds(); 
- 		Retardo(2500000); 
- 		GPIOD_PDOR = 0xFFFFFFFD; // 0xFFFFFFFD AZUL
- 		Retardo(2500000); 
-		apagarLeds(); 
- 		Retardo(2500000); 
- 		 
- 	} 
- 	 
- 	return 0; 
- } 
- 
- 
- void apagarLeds(void) 
- { 
- 	portB = 0xFFFFFFFF; // PORT B ROJO Y VERDE
- 	GPIOD_PDOR = 0xFFFFFFFF; // PORT D AZUL
- } 
+void cfgPorts(void);
+void Delay(long miliseg);
 
- void Retardo(long miliseg) 
-  { 
-  	do 
-  	{ 
- 		miliseg=miliseg -1; 
-  	} 
- 	while(miliseg!=0); 
- 	 
-  } 
 
- void cfgPorts(void)
- {  	 	  
-	 SIM_SCGC5 =  SIM_SCGC5_PORTB_MASK;
-	 SIM_SCGC5 |=  SIM_SCGC5_PORTA_MASK; 
-	 SIM_SCGC5 |=  SIM_SCGC5_PORTD_MASK;
-	 	
-	 //ACTIVA EL MODO DEL PIN 
-	 PORTB_PCR18 = PORT_PCR_MUX(1);// 1 es el modulo de la tabla la clumna de funcionalidad
-	 PORTB_PCR19 = PORT_PCR_MUX(1);
-	 PORTD_PCR1  = PORT_PCR_MUX(1);// MODO 1
-	 
-	 
-	 //CONFIGURE PORT B AS OUTPUT           para encontrar lo siguiente buscar capitulo GPIO
- 	GPIOB_PDDR= 0xFFFFFFFF; 
- 	GPIOD_PDDR= 0xFFFFFFFF; 
- } 
- 
- 
- 
+int main(void)
+{
+	cfgPorts();
+	portB=turnLedsOff;
+	portD=turnLedsOff;
+	Delay(oneSec);
+	
+	for(;;) 
+	{
+		
+		if(!(portBIn & (0x00000001)))
+		{
+			portB=turnRedLedOn;
+		}
+		if(!(portBIn & (0x00000002)))
+		{
+			portB=turnGreenLedOn;
+		}
+		if(!(portBIn & (0x00000004)))
+		{
+			portD=turnBlueLedOn;
+		}
+		portB=turnLedsOff;
+		portD=turnLedsOff;
+		/*else
+		{
+			portB=turnLedsOff;
+		}
+		*/
+		/*
+		portB = 0xFFFBFFFF; //ROJO
+		Delay(1800000);
+		apagarLeds();
+		Delay(1800000);
+		portB = 0xFFF7FFFF; //VERDE
+		Delay(1800000);
+		apagarLeds();
+		Delay(1800000);
+		portD = 0xFFFFFFFD; //AZUL
+		Delay(1800000);
+		apagarLeds();
+		Delay(1800000);*/
+		
+	}
+	
+	return 0;
+}
+
+
+void cfgPorts(void)
+{
+	// TURN ON CLOCK´S
+	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	
+	// TURN ON PIN MODE
+	PORTB_PCR0= PORT_PCR_MUX(1);
+	PORTB_PCR1= PORT_PCR_MUX(1);
+	PORTB_PCR2= PORT_PCR_MUX(1);
+	
+	PORTB_PCR18= PORT_PCR_MUX(1);
+	PORTB_PCR19= PORT_PCR_MUX(1);
+	PORTD_PCR1= PORT_PCR_MUX(1); 
+	
+	
+	// CONFIGURE PORTB FROM 31 TO 3 AS OUTPUT AND 0-2 AS INPUT
+	GPIOB_PDDR= 0xFFFFFFF8;
+	GPIOD_PDDR= 0xFFFFFFFF;
+}
+
+void Delay(long miliseg)
+{
+	do
+	{
+		miliseg=miliseg-1;
+	}
+	while(miliseg!=0);
+	
+}
