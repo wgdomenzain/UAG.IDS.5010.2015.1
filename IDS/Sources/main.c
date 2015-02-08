91e844706aa1833a7dfeaef08cc904e08d7a9140
Dachb <dachb_10@hotmail.com>
2015-02-08 19:47:35 +0100

    Encender Leds Rojo,Vede y Azul con botones

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,62 +1,101 @@
+
 /*
  * main implementation: use this 'C' sample to create your own application
  *
  */
 #include "derivative.h" /* include peripheral declarations */
 
+#define portBIn GPIOB_PDIR
+#define portB GPIOB_PDOR
+#define portD GPIOD_PDOR
+#define turnRedLedOn 0xFFFBFFFF
+#define turnBlueLedOn 0xFFFFFFFD
+#define turnGreenLedOn 0xFFF7FFFF
+#define turnLedsOff  0xFFFFFFFF
+#define oneSec 1600000
+
+
 void cfgPorts(void);
-void Delay(long ms);
-void apagarLeds(void);
+void Delay(long miliseg);
+
+
 int main(void)
 {
 	cfgPorts();
-	apagarLeds();
+	portB=turnLedsOff;
+	portD=turnLedsOff;
+	Delay(oneSec);
 	
-	for(;;) {	   
-		GPIOB_PDOR = 0xFFFBFFFF; 
+	for(;;) 
+	{
+		
+		if(!(portBIn & (0x00000001)))
+		{
+			portB=turnRedLedOn;
+		}
+		if(!(portBIn & (0x00000002)))
+		{
+			portB=turnGreenLedOn;
+		}
+		if(!(portBIn & (0x00000004)))
+		{
+			portD=turnBlueLedOn;
+		}
+		portB=turnLedsOff;
+		portD=turnLedsOff;
+		/*else
+		{
+			portB=turnLedsOff;
+		}
+		*/
+		/*
+		portB = 0xFFFBFFFF; //ROJO
 		Delay(1800000);
 		apagarLeds();
 		Delay(1800000);
-		GPIOB_PDOR = 0xFFF7FFFF; 
+		portB = 0xFFF7FFFF; //VERDE
 		Delay(1800000);
 		apagarLeds();
 		Delay(1800000);
-		GPIOD_PDOR = 0xFFFFFFFD; 
+		portD = 0xFFFFFFFD; //AZUL
 		Delay(1800000);
 		apagarLeds();
-		Delay(1800000);
+		Delay(1800000);*/
 		
 	}
 	
 	return 0;
 }
 
-void apagarLeds(void)
-{
-	GPIOB_PDOR = 0xFFFFFFFF; 
-	GPIOD_PDOR = 0xFFFFFFFF; 
-}
+
 void cfgPorts(void)
 {
-	
+	// TURN ON CLOCK´S
 	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
 	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
 	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
-
+	
+	// TURN ON PIN MODE
+	PORTB_PCR0= PORT_PCR_MUX(1);
+	PORTB_PCR1= PORT_PCR_MUX(1);
+	PORTB_PCR2= PORT_PCR_MUX(1);
+	
 	PORTB_PCR18= PORT_PCR_MUX(1);
 	PORTB_PCR19= PORT_PCR_MUX(1);
 	PORTD_PCR1= PORT_PCR_MUX(1); 
 	
-	GPIOB_PDDR= 0xFFFFFFFF;
+	
+	// CONFIGURE PORTB FROM 31 TO 3 AS OUTPUT AND 0-2 AS INPUT
+	GPIOB_PDDR= 0xFFFFFFF8;
 	GPIOD_PDDR= 0xFFFFFFFF;
 }
 
-void Delay(long ms)
+void Delay(long miliseg)
 {
 	do
 	{
-		ms--;
+		miliseg=miliseg-1;
 	}
-	while(ms!=0);
+	while(miliseg!=0);
 	
 }
