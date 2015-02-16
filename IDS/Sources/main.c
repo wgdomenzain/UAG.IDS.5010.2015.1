6adc83ecd38ae7e0cd53da6954eb0b705d5e5eb0
CarlosPayan <carlospayan26@gmail.com>
2015-02-16 10:39:29 -0600

    LCD

--- IDS/Sources/main.c
+++ IDS/Sources/main.c
@@ -1,81 +1,180 @@
 /*
- * main implementation: use this 'C' sample to create your own application
- *
- */
-
-
-
-
+@author: 		Walter Gonzalez Domenzain
+@description: 	8020 Group
+@functions:		None
+@environment: 	KL25Z
+@date: 			22/01/2014
+@comments:		
+@version:		1.0 - Initial
+*/
 
 #include "derivative.h" /* include peripheral declarations */
-#define portB 			GPIOB_PDOR
-#define portD 			GPIOD_PDOR
-#define TurnRedLedOn 	0xFFFBFFFF
-#define TurnGreenLedOn 	0xFFF7FFFF
-#define TurnBlueLedOn 	0xFFFFFFFD
-#define TurnLedsOff 	0xFFFFFFFF
-//time constant
-#define n1sec			1600000
-#define n2sec			n1sec*2
-#define nhalfsec		n1sec/2
-void configPorts(void);
+
+
+#define GPIO_PIN_MASK 0x1Fu
+#define GPIO_PIN(x) (((1)<<(x & GPIO_PIN_MASK)))
+
+#define	nBit0	0x01	//'00000001'
+#define	nBit1	0x02	//'00000010'
+#define	nBit2	0x04	//'00000100'
+#define	nBit3	0x08	//'00001000'
+#define	nBit4	0x10	//'00010000'
+#define	nBit5	0x20	//'00100000'
+#define	nBit6	0x40	//'01000000'
+#define	nBit7	0x80	//'10000000'
+
+//Time definitions
+#define nt15_msec	3500
+#define nt40_usec	35
+
+//LCD Control
+#define nIns	0
+#define nData	1
+
+#define PortLCD    	GPIOC_PDOR
+//Enable connected to portb_01
+#define Enable_1	GPIOB_PDOR |= 0x01
+#define Enable_0	GPIOB_PDOR &= 0xFE
+#define RS_1   		GPIOB_PDOR |= 0x02
+#define RS_0   		GPIOB_PDOR &= 0xFD
+
+
+
+
+#define	Set_GPIOB_PDOR(x)	(GPIOB_PDOR |= (1 << (x-1)))
+
+int int_Temp;
+
+//Cursor Blink off initialization
+const unsigned char InitializeLCD[5] = {0x38, 0x38, 0x38, 0x0C, 0x01};
+//--------------------------------------------------------------
+//Declare Prototypes
+/* Functions */
+void cfgPorts(void);
+void initLCD(void);
+void delay(long time);
+void sendCode(int Code, int Data);
+
+/*@description: Initial Port Cfg 
+*/
+			
 int main(void)
 {
-	//Ports configuration
-	configPorts();
-	portB= TurnLedsOff;
-	portD= TurnLedsOff;
+	//Configure ports
+	cfgPorts();
+	//Initialize LCD
+	initLCD();
+	//Set position to print character
+	sendCode(nIns, 0x80);
+	//Print characters
 	
+	sendCode(nData, 'C');
+	sendCode(nData, 'A');
+	sendCode(nData, 'R');
+	sendCode(nData, 'L');
+	sendCode(nData, 'O');
+	sendCode(nData, 'S');
+	sendCode(nData, ' ');
+	sendCode(nData, 'P');
+	sendCode(nData, 'A');
+	sendCode(nData, 'Y');
+	sendCode(nData, 'A');
+	sendCode(nData, 'N');
+
 	for(;;)
-	{	  
-		if(!(GPIOB_PDIR & (0x00000001)))
-		{
-			portB = TurnRedLedOn;		
-		}
-		if(!(GPIOB_PDIR & (0x00000002)))
-		{
-			portB = TurnGreenLedOn;		
-		}
-		if(!(GPIOB_PDIR & (0x00000004)))
-		{
-			portD = TurnBlueLedOn;		
-		}
-		portB= TurnLedsOff;
-		portD= TurnLedsOff;
-			
+	{
+ 
 	}
 	
 	return 0;
 }
-void configPorts(void)
+
+void cfgPorts(void)
 {
-	//Turn clock on
-	//SIM_SCG5 = 0X00000400;
-	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;
-	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;
-	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	//Turn on clock for portb
+	SIM_SCGC5 = SIM_SCGC5_PORTB_MASK;	
+	//Turn on clock for portd
+	//SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;	
+	////Turn on clock for portc
+	SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
+	
+	/* Set pins of PORTB as GPIO */
+	PORTC_PCR0 = PORT_PCR_MUX(1);
+	PORTC_PCR1 = PORT_PCR_MUX(1);
+	PORTC_PCR2 = PORT_PCR_MUX(1);
+	PORTC_PCR3 = PORT_PCR_MUX(1);
+	PORTC_PCR4 = PORT_PCR_MUX(1);
+	PORTC_PCR5 = PORT_PCR_MUX(1);
+	PORTC_PCR6 = PORT_PCR_MUX(1);
+	PORTC_PCR7 = PORT_PCR_MUX(1);
+	
+	/* Set pins of PORTC as GPIO */
+	PORTB_PCR0= PORT_PCR_MUX(1);
+	PORTB_PCR1= PORT_PCR_MUX(1);
+	
+	
+	//Initialize PortB
+	GPIOB_PDOR = 0x00;
+	
+	//Initialize PortC
+	GPIOC_PDOR = 0X00;
 
-	PORTB_PCR0 = PORT_PCR_MUX(1);
-	PORTB_PCR1 = PORT_PCR_MUX(1);
-	PORTB_PCR2 = PORT_PCR_MUX(1);
-	PORTB_PCR18 = PORT_PCR_MUX(1);
-	PORTB_PCR19 = PORT_PCR_MUX(1);
+	//Configure PortB as outputs
+	GPIOB_PDDR = 0xFF;
 	
-	PORTD_PCR1 = PORT_PCR_MUX(1);
+	//Configure PortD as outputs
+	//GPIOD_PDDR = 0xFF;
 	
-	//Configure portB  B0 to B2 as input
-	//GPIOB_PDDR = 0xFFFFFFFF; //1111 1111 1111 1111
-	
-	GPIOB_PDDR = 0xFFFFFFF8;  
-	GPIOD_PDDR = 0xFFFFFFFF;
-
-	//SIM_SCG5
+	//Configure PortC as outputs
+	GPIOC_PDDR = 0xFF;
 }
-void delay (long time)
+
+void initLCD(void)
 {
 	int i;
-	for(i=0;i<time;i++)
+	delay(nt15_msec);
+	
+	/* Send initialization instructions */
+	/* Loop for sending each character from the array */
+	for(i=0;i<5;i++)
+	{										
+		sendCode(nIns, InitializeLCD[i]);	/* send initialization instructions */
+	}
+	
+}
+
+void sendCode(int Code, int Data)
+{
+	//Assign a value to pin RS
+	/*HINT: When RS is 1, then the LCD receives a data
+	when RS is 0, then the LCD receives an instruction */
+	// Initialize RS and Enable with 0
+	RS_0;
+	Enable_0;
+	//Assign the value we want to send to the LCD
+	PortLCD = Data;	
+	
+	//We make the algorithm to establish if its an instruction we start with 0 on RS value, otherwise if its a data command we start with RS as 1;
+	if (Code == nIns)
 	{
-		
+		Enable_1;
+		delay(nt40_usec);
+		Enable_0;
+		RS_0;
+	}		
+	else if(Code == nData)
+	{
+		RS_1;
+		Enable_1;
+		delay(nt15_msec);
+		Enable_0;
+		RS_0;
 	}
 }
+void delay(long time)
+{
+	while (time > 0)
+	{
+		time--;
+	}
+}                                                                                                                                                                                          
