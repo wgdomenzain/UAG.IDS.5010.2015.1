a9472751d5cbbb761c1100628a5c9b1c051e8dc6
porritas <porrasporrasporras@gmail.com>
2015-02-25 14:48:55 -0600

    PWM
    @13.1msPERIOD @Channel0

--- IDS/FLASH/Project_Settings/Startup_Code/__arm_start.d
+++ IDS/FLASH/Project_Settings/Startup_Code/__arm_start.d
@@ -20,7 +20,7 @@ Project_Settings/Startup_Code/__arm_start.o: \
  C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
  C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
  C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
- C:/UAG/Projects/UAG.IDS.5010.2015.1/IDS/Project_Settings/Startup_Code/runtime_configuration.h
+ C:/UAG/Projects/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/IDS/Project_Settings/Startup_Code/runtime_configuration.h
 
 C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
 
@@ -62,4 +62,4 @@ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
 
 C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
 
-C:/UAG/Projects/UAG.IDS.5010.2015.1/IDS/Project_Settings/Startup_Code/runtime_configuration.h:
+C:/UAG/Projects/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/IDS/Project_Settings/Startup_Code/runtime_configuration.h:
--- IDS/FLASH/Sources/main.d
+++ IDS/FLASH/Sources/main.d
@@ -2,8 +2,8 @@ Sources/main.o: ../Sources/main.c \
  C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
  C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
  C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
- C:/UAG/Projects/UAG.IDS.5010.2015.1/IDS/Project_Headers/derivative.h \
- C:/UAG/Projects/UAG.IDS.5010.2015.1/IDS/Project_Headers/MKL25Z4.h \
+ C:/UAG/Projects/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/IDS/Project_Headers/derivative.h \
+ C:/UAG/Projects/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/IDS/Project_Headers/MKL25Z4.h \
  C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
  C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
  C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
@@ -33,9 +33,9 @@ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
 
 C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
 
-C:/UAG/Projects/UAG.IDS.5010.2015.1/IDS/Project_Headers/derivative.h:
+C:/UAG/Projects/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/IDS/Project_Headers/derivative.h:
 
-C:/UAG/Projects/UAG.IDS.5010.2015.1/IDS/Project_Headers/MKL25Z4.h:
+C:/UAG/Projects/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/UAG.IDS.5010.2015.1/IDS/Project_Headers/MKL25Z4.h:
 
 C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
 
